cbc3ec8185f8cda68b3571268a630c388325cb28
blazej-smorawski <blazej.smorawski@gmail.com>
2022-12-09 22:04:20 +0100

    Add parsing arguments

--- benchmark.go
+++ benchmark.go
@@ -7,8 +7,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
+
+	"github.com/akamensky/argparse"
 )
 
 type CreateSessionRequestData struct {
@@ -20,7 +21,7 @@ type SendCodeRequestData struct {
 	Code     []string `json:"code"`
 }
 
-func start_session(url *string, id uint64, c chan float64) {
+func start_session(url *string, id int, c chan float64) {
 	username := fmt.Sprintf("user-%d", id)
 	user := CreateSessionRequestData{
 		Username: username,
@@ -49,7 +50,6 @@ func start_session(url *string, id uint64, c chan float64) {
 	fmt.Printf("user - %d\tresponse(/start_session) - %d\telapsed - %s\n", id, response.StatusCode, elapsed)
 
 	if response.StatusCode != 200 {
-		// Print the request body
 		body, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			fmt.Println(err)
@@ -69,7 +69,7 @@ func start_session(url *string, id uint64, c chan float64) {
 	c <- elapsed.Seconds()
 }
 
-func send_code(url *string, id uint64, runs uint64, c chan float64) {
+func send_code(url *string, id int, runs int, c chan float64) {
 	username := fmt.Sprintf("user-%d", id)
 	code := SendCodeRequestData{
 		Username: username,
@@ -82,8 +82,9 @@ func send_code(url *string, id uint64, runs uint64, c chan float64) {
 		return
 	}
 
-	var average_time float64 = 0
 	client := &http.Client{}
+	var average_time float64 = 0
+
 	for i := 0; i < int(runs); i++ {
 		request, err := http.NewRequest("POST", *url+"/send_code", bytes.NewBuffer(jsonData))
 		request.Header.Set("Content-Type", "application/json; charset=UTF-8")
@@ -119,46 +120,59 @@ func send_code(url *string, id uint64, runs uint64, c chan float64) {
 
 		average_time += elapsed.Seconds()
 	}
+
 	fmt.Printf("%f\n", (average_time / float64(runs)))
 	c <- (average_time / float64(runs))
 }
 
 func main() {
-	args := os.Args[1:]
+	parser := argparse.NewParser("lynx-benchmark", "Benchmark mainly lynx-runner + lynx-runtime")
+
+	url := parser.StringPositional(&argparse.Options{Default: "http://server.blazej-smorawski.com", Help: "Url that will be queried at `/start_session` and `/send_code`"})
+	threads_ptr := parser.Int("t", "threads", &argparse.Options{Default: 8, Help: "Count of threads to be spawned which will send parallel queries to `runner`"})
+	runs_ptr := parser.Int("r", "runs", &argparse.Options{Default: 8, Help: "Number of times each `send_code` query should be repeated by each thread"})
+	only_code_ptr := parser.Flag("c", "only-code", &argparse.Options{Help: "Do not create new sessions, just use `send_code`"})
+
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
+	threads := *threads_ptr
+	runs := *runs_ptr
+	only_code := *only_code_ptr
 
 	var c chan float64 = make(chan float64)
-	var url string = args[0]
-
-	thread_count, err := strconv.ParseUint(args[1], 10, 64)
-	runs_count, err := strconv.ParseUint(args[2], 10, 64)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for i := uint64(0); i < thread_count; i++ {
-		go start_session(&url, i, c)
-		time.Sleep(1 * time.Second)
+	if !only_code {
+		for i := 0; i < threads; i++ {
+			go start_session(url, i, c)
+		}
+
+		var create_session_avg float64 = 0
+		for i := 0; i < threads; i++ {
+			create_session_avg += <-c
+		}
+		create_session_avg /= float64(threads)
+
+		fmt.Printf("Average session creation time: %fs\n", create_session_avg)
 	}
 
-	var create_session_avg float64 = 0
-	for i := uint64(0); i < thread_count; i++ {
-		create_session_avg += <-c
-	}
-	create_session_avg /= float64(thread_count)
-
-	fmt.Printf("Average session creation time: %fs\n", create_session_avg)
-
-	for i := uint64(0); i < thread_count; i++ {
-		go send_code(&url, i, runs_count, c)
+	for i := 0; i < threads; i++ {
+		go send_code(url, i, runs, c)
 	}
 
 	var send_code_avg float64 = 0
-	for i := uint64(0); i < thread_count; i++ {
+	for i := 0; i < threads; i++ {
 		send_code_avg += <-c
 	}
-	send_code_avg /= float64(thread_count)
+	send_code_avg /= float64(threads)
 
 	fmt.Printf("Average code execution time: %fs\n", send_code_avg)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module benchmark
 
 go 1.18
+
+require github.com/akamensky/argparse v1.4.0 // indirect
